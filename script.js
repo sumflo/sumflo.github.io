09dd0b5b1ec39fb0e880e73e9a451b74a6b75724
sumflo <real61madrid@gmail.com>
2022-10-16 17:19:54 +0200

    design change

---
+++ script.js
@@ -0,0 +1,62 @@
+const hobbyProjects = [
+    {
+        id: 1,
+        title: 'Anakin',
+        text: 'This was my first resume-like website that I made during the #MyFristApp Coding Camp.',
+        image: 'images/anakin.PNG',
+        ref: '/pages/anakin/anakin.html',
+    },
+    {
+        id: 2,
+        title: 'Baby',
+        text: 'I created this website during the #MyFirstApp Coding Camp. My little boy inspired me. The site is about him. He motivated me to become a programmer, providing him with a better future and financial security.',
+        image: 'images/baby.PNG',
+        ref: '/pages/nimi/babyCv.html'
+    },
+    {
+        id: 3,
+        title: 'MyFirstApp',
+        text: 'The first app I made. The final project of #MyFirstApp Coding Camp. This is how I understood the essence of programming. This was my main practice project.',
+        image: 'images/myFirstApp.PNG',
+        ref: '/pages/MyFirstApp/index.html',
+    },
+    {
+        id: 4,
+        title: 'Wonders of Nature',
+        text: 'This project was a homework assignment that a dear classmate of mine and I did together.',
+        image: 'images/wonders.PNG',
+        ref: '/pages/TheWondersOfNature/index.html',
+    },
+    {
+        id: 5,
+        title: 'My Website',
+        text: 'This project I made it as part of dr. Angels Yu - The Complete 2022 Web Development Bootcamp.',
+        image: 'images/myWebsite.PNG',
+        ref: '/pages/MyWebsite/index.html',
+    },
+]
+
+let displayHobbyProjects = (element) => {
+
+    if(element.id % 2 !== 0){
+        $('.websiteDescriptions')
+            .append('<div class="sectionContainer"><a href=""><img src="" alt="Webpage representation." class="hobbyProject"><div class="article"></a><h3 class="websiteTitle"></h3><p class="websiteText"></p></div></div>');
+
+        $('.websiteDescriptions .sectionContainer:last a').attr('href', element.ref);
+        $('.websiteDescriptions .sectionContainer:last a img').attr('src', element.image);
+        $('.websiteDescriptions .sectionContainer:last div .websiteTitle').text(element.title);
+        $('.websiteDescriptions .sectionContainer:last div .websiteText').text(element.text);
+    }else{
+        $('.websiteDescriptions')
+            .append('<div class="sectionContainer"><div class="article"><h3 class="websiteTitle"></h3><p class="websiteText"></p></div><a href=""><img src="" alt="Webpage representation." class="hobbyProject"></a></div>');
+
+        $('.websiteDescriptions .sectionContainer:last a').attr('href', element.ref);
+        $('.websiteDescriptions .sectionContainer:last img').attr('src', element.image);
+        $('.websiteDescriptions .sectionContainer:last div .websiteTitle').text(element.title);
+        $('.websiteDescriptions .sectionContainer:last div .websiteText').text(element.text);
+    }
+
+
+}
+
+hobbyProjects.forEach(displayHobbyProjects)
